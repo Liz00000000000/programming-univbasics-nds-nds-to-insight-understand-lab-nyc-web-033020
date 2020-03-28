9a51afbac35695e54510717e3ba56a67b1510493
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-28 23:01:05 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-    directors_database[:movies][:title].each do
-      puts directors_database[:movies][:title]
+    directors_database[:movies][:title].each do |movie|
+      puts movie 
     end
 end
